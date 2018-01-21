a97a7fdde6eda41c9495ad264b37b7ac0f1dd03a
PeaceJeff <jefimhelp@gmail.com>
2018-01-21 23:07:12 +0200

    added some finishing corrections with code readibility added few comments and did some more testing on 3 major brosers Chrome FireFox and IE11

--- webApp/README.md
+++ webApp/README.md
@@ -5,3 +5,9 @@ Download webApp folder localy and run webApp/index.html in any browser.
 
 ## Application compatibility
 The application was tested and fully functional on Chrome, FireFox and IE11.
+
+## Application was tests
+Application was tested with various illegal User interaction like:
+1. Invalid characters,
+1. Huge numbers,
+1. Pasting information with Ctrl+V command.
--- webApp/js/main.js
+++ webApp/js/main.js
@@ -1,48 +1,66 @@
 getCurrencyList();
-const CURRDECIMAL = 2;
-var currency = '';
+const CURRDECIMAL = 2; //this controls the number of symbols after decimal. Did not setup initialisation for this.
+var testing = false; //In order to see input identification messages for testing change to 'true'
+var sendInput = $('#sendCurrInput').val(); 
+var receiveInput = $('#receiveCurrInput').val();
 
-$(document).ready(function(){
+// all the user input configuration was based of WUPOS currency input field example.
+// Not sure if this is the best practice, but it works in order to avoid invalid entries.
+
+$(document).ready(function(){	
+	// upon engaging the input field cursor is throw to the back off it.
+	$('input').on('click',function(){
+		var input = document.getElementById($(this).attr('id'));
+		input.focus();
+		input.setSelectionRange($(this).val().length,$(this).val().length);
+	});
+	
 	$('#sendSelection, #receiveSelection').change(function() {
 		// update icon for Sender
 		$(this).parent().find('div').empty();
 		$(this).parent().find('div').append($(this).parent().find('select option:selected').attr('icon'));
-		// update currency on different selection
-		/*
-		if($(this).attr("id") == "sendSelection"){
-			calculationSend();
-		}else{
-			calculationReceive();
-		}
-		*/
-		// I think previous implementation was a bit confusing for user in terms of functionality
-		// so I decided on a better solution below:
-		calculationSend();
-		// Sending Input field will only change when interacting with #receiveCurrInput
+		calculationSend();// Sending Input field will only change when interacting with #receiveCurrInput
 	})
 
 	
 	$('#sendCurrInput, #receiveCurrInput').keyup(function(e){
 		//limit only for numeric characters
 		if((e.which > 47)&&(e.which < 57)||(e.which > 95)&&(e.which < 105)){
+			if(testing)console.log('Valid Number enetered');
 			var actualValue = parseFloat($(this).val())*10;
 			$(this).val(parseFloat(actualValue).toFixed(CURRDECIMAL));
+		
 		//control pasting text or when nothing is entered
-		}else if((isNaN($(this).val()))){
-			$(this).val(parseFloat('0.00'));
+		}else if(isNaN($(this).val())){
+			if(testing)console.log('CTRL+V of illegal text or any other illegal character detected');
+			if ($(this).attr('id') == 'sendCurrInput'){
+				$(this).val(sendInput);
+			}else{
+				$(this).val(receiveInput);
+			}
+			
 		//controlling backspace
 		}else if(e.which == 8){
+			if(testing)console.log('Backspace detected');
 			if($(this).val()==''){
 				$(this).val(parseFloat('0.00'));
 			}
 			var actualValue = parseFloat($(this).val())/10;
 			$(this).val(parseFloat(actualValue).toFixed(CURRDECIMAL));
-		//just make sure that anything that was entered is parsed to control the datatype.
+			
+		//just make sure that anything invalidly entered would be returned as a previous value.
 		}else{
-			console.log('Invalid Characters detected');
+			if(testing)console.log('Invalid Characters detected');
 			$(this).val(parseFloat($(this).val()).toFixed(CURRDECIMAL));
+			/*if ($(this).attr('id') == 'sendCurrInput'){
+				console.log(sendInput)
+				$(this).val(sendInput);
+			}else{
+				console.log(receiveInput)
+				$(this).val(receiveInput);
+			}*/
 		}
-		console.log(e.which);
+		//console.log(e.which);
 		
 		// after initialisation is done calculate
 		if($(this).attr("id") == 'sendCurrInput'){
@@ -71,9 +89,14 @@ function getCurrencyList(){
 			//console.log(countryCodeList);
 			CplusCList = createCurrencyPlusCountryList(currencyList,countryCodeList);
 			add2Option(CplusCList,'#sendSelection','#currSendIcon');
+			//initialisation for first option to be selected as EURO.
+			$("#sendSelection option:last").attr("selected", "selected");
 			add2Option(CplusCList,'#receiveSelection','#currReceiveIcon');
 		});
-	 });
+	 }).fail(function() { alert('This application has lost connection to internet or the to source of information, '
+		+'could you please try checking your connectivity or this link reachablity: https://api.fixer.io/latest . '
+		+'\n\nThis is a legit message that was thought through by Jefim and implemented/tested specifically for show off.' 
+		+'\n\nIn order to be able to test this functionality try to disable your internet or rip out the internet cord from your computer :)')});
 }
 
 function add2Option(CPCList, selector,icon){
@@ -98,18 +121,22 @@ function createCurrencyPlusCountryList(cL, cCL){
 	return newList;
 }
 
-
-// these two last functions are very similar 
+// These two last functions are very similar, but cannot afford extra time to optimize.
 function calculationSend(){
 	//since the calculation is based on a EUR I need to devide and multiple after
 	var sendC = parseFloat($('#sendSelection option:selected').val());
 	var receiveC = parseFloat($('#receiveSelection option:selected').val());
 	var currResult = parseFloat($('#sendCurrInput').val())/sendC*receiveC
 	$('#receiveCurrInput').val(currResult.toFixed(CURRDECIMAL));
+	sendInput = $('#sendCurrInput').val();
+	receiveInput = $('#receiveCurrInput').val();
 }
+
 function calculationReceive(){
 	var sendC = parseFloat($('#sendSelection option:selected').val());
 	var receiveC = parseFloat($('#receiveSelection option:selected').val());
 	var currResult = parseFloat($('#receiveCurrInput').val())/receiveC*sendC
 	$('#sendCurrInput').val(currResult.toFixed(CURRDECIMAL));
+	sendInput = $('#sendCurrInput').val();
+	receiveInput = $('#receiveCurrInput').val();
 }
